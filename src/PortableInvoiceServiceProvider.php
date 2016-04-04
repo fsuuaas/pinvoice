c1ee6503b694422543eed700d98ac75d8fab249a
Kaan Kılıç <bl4cksta@gmail.com>
2016-04-04 22:35:41 +0300

    Init project

---
+++ src/PortableInvoiceServiceProvider.php
@@ -0,0 +1,24 @@
+<?php 
+namespace \Kaankilic\PortableInvoice\src;
+use Illuminate\Support\ServiceProvider;
+class PortableInvoiceServiceProvider extends ServiceProvider {
+  
+   /**
+     * Bootstrap the application services.
+     *
+     * @return void
+    */
+  public function boot(){
+  }
+ 
+  /**
+    * Register the application services.
+    *
+    * @return void
+  */
+  public function register()
+  {
+   //
+  }
+ 
+}
